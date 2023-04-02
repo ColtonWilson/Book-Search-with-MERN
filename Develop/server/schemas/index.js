33e9470e1a57a524a74eb7bc6c8479f70f85d003
Colton Wilson <colton_wilson93@yahoo.com>
2023-04-02 09:48:18 -0700

    Working on server

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,5 @@
+//Skeleton code help from module 21 lesson 25 server/schemas/index.js
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,69 @@
+//Skeleton code help from module 21 lesson 25 server/schemas/resolvers.js
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await Profile.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { book }, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        return await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book } },
+        { new: true}
+      );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    deleteBook: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOneAndUpdate(
+            {_id: context.user._id },
+            { $pull: { savedBooks: { bookId: params.bookId } } },
+            { new: true },
+             );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+  },
+};
+
+module.exports = resolvers;
